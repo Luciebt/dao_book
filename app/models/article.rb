e943d1cf38314494e87ea33a4a7feeb4ff018997
lucie brismontier <luciebtdev@gmail.com>
2020-01-19 00:46:37 +0100

    modif article form

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,10 @@
 class Article < ActiveRecord::Base
-  has_and_belongs_to_many :categories
   validates :title, presence: true
   validates :content, presence: true
 
   def markdown_content
     Kramdown::Document.new(content).to_html.html_safe
   end
+
+  CATEGORIES = ["Privacy", "Security", "Liability", "Laws"]
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,3 @@
 class Category < ApplicationRecord
-  has_and_belongs_to_many :articles
   validates :name, presence: true
 end
